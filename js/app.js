43dcbd10f5f191aa402ea762e61f830feecba14c
marcelo vinicius <viniciusmarcello12@gmail.com>
2021-08-26 22:33:39 -0300

    adicionando animes com API

--- js/app.js
+++ js/app.js
@@ -1,28 +1,42 @@
-function carregarAnimes() {
-  let objetoAnimes = [
-      {
-          foto: "pokemon.jpg",
-          nome: "pokemon"
-      },
-      {
-        foto: "naruto.png",
-        nome: "naruto"
-      },
-      {
-      foto : "dragon.jpg",
-      nome: "dragon ball z"
+function ConsultaAPI(){
+  const animeAPI = "https://api.jikan.moe/v3/season/2021/spring";
+  let objetoAnimes = [];
+   $.ajax({
+     url : animeAPI,
+     async : false,
+     success : function(dados){
+       objetoAnimes = dados.anime;
+     }
+   });
+   return objetoAnimes;
   }
-]
-let listaAnimes = document.querySelector(".lista-animes");
-let cartao = "";
-for (let i = 0; i < objetoAnimes.length; i++){
-    cartao += "<div class='cartao'>";
-    cartao += "<img src='img/" + objetoAnimes[i].foto + "'>";
-    cartao += objetoAnimes[i].nome;
-    cartao += "</div>"
-}
-listaAnimes.innerHTML= cartao;
-}
-window.onload = function(){
-    carregarAnimes();
-}
+   ConsultaAPI();
+ 
+ function MontarCartao(foto, nome , score , url){
+   let cartao = "";
+   cartao += "<div class='cartao'>";
+   cartao += "<img src='" + foto + "'>";
+   cartao += nome ;"<br>"
+   cartao += "<br>"
+   cartao += "nota score = " + score  ;"<br>"
+   cartao += "<br>"
+   cartao += "<li><a href=" + url + ">veja mais</a></li>";
+   cartao += "</div>";
+   return cartao;
+ }
+ function carregarAnimes(){
+   let listaAnimes = document.querySelector(".lista-animes");
+   let conjuntoDeCartoes = "";
+   let TodosAnimes = ConsultaAPI(); 
+ 
+ 
+   TodosAnimes.forEach(animeAtivo =>{
+     conjuntoDeCartoes += MontarCartao(animeAtivo.image_url, animeAtivo.title , animeAtivo.score , animeAtivo.url);
+   });
+ 
+ listaAnimes.innerHTML= conjuntoDeCartoes;
+ }
+ let botao = document.querySelector("#carregar");
+ botao.onclick = function(){
+     carregarAnimes();
+ }
